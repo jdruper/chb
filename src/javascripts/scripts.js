9c17c58b155cd110126a9c95b346e18233bbbdf2
Jose Daniel Beita <jbeita@onetrust.com>
2021-11-11 12:56:12 -0600

    Add mobile menu

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,1 +1,16 @@
 // Add your scripts here
+
+let isMenuOpen = false;
+
+function toggleMenu() {
+  isMenuOpen = !isMenuOpen;
+  const menu = document.querySelector('.mobile-menu');
+  if (isMenuOpen) {
+    menu.classList.add('mobile-menu-display');
+    return;
+  }
+  menu.classList.remove('mobile-menu-display');
+}
+
+document
+  .querySelectorAll('.mobile-menu-icon').forEach((menuIcon) => menuIcon.addEventListener('click', toggleMenu));
