b01b474e128f6ab0feb6c7b2841303baefe209e3
Jose Beita <jdruper@gmail.com>
2021-11-12 18:30:22 -0600

    Add Contacto page

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -12,5 +12,21 @@ function toggleMenu() {
   menu.classList.remove('mobile-menu-display');
 }
 
+function copyMail() {
+  navigator.clipboard.writeText('hola@hierbabuena.cr');
+  document.querySelector('.copy-icon').classList.add('hide');
+  document.querySelector('.success-icon').classList.remove('hide');
+  setTimeout(() => {
+    document.querySelector('.copy-icon').classList.remove('hide');
+    document.querySelector('.success-icon').classList.add('hide');
+    return true;
+  }, 5000);
+}
+
 document
   .querySelectorAll('.mobile-menu-icon').forEach((menuIcon) => menuIcon.addEventListener('click', toggleMenu));
+
+const copy = document.querySelector('.contacto__copy');
+if (copy) {
+  copy.addEventListener('click', copyMail);
+}
